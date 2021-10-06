e4821319b9595e6dfd8927aff0c91165b325dd9b
JakeWill5 <jacob.m.williams100@gmail.com>
2021-10-06 14:27:17 -0700

    please work

---
+++ RUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+
+            while (repeat)
+
+        
+            {
+                //clean the screen on the terminal
+                Console.Clear();
+                Console.WriteLine("Hello World!");
+                string uderChoice;
+                Console.WriteLine("[1] - Do you want to clear the screen?")
+                Console.WriteLine("[2] - Do you want to excit out of the program?");
+                userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine("User has chosen to clear the screen");
+                        Console.WriteLine("Press Enter to constinue...");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        repeat = false;
+                        Console.WriteLine("You have exited to the program!");
+                        Console.WriteLine("Press Enter to continue...");
+                        Console.ReadLine();
+
+                    default:
+                        Console.WriteLine("What the heck did you just type, try again");
+                        Console.WriteLine("Press Enter to continue...");
+                        Console.ReadLine();
+                        break;
+                }
+            }    
+       }
+    }
+}
---
+++ RestaurantReview/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _RestaurantReview
+{
+    public class Class1
+    {
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set 
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    //Will give the user an exception whenevr you give
+                    throw new Exception("City can only hold letters!");
+                }
+                _city = value;
+            }
+        }
+    }
+}
+           
+      
